3aa827afde780bc2c18084634b16e41e37bd13e5
Akram <akramshafeek70@gmail.com>
2023-10-10 12:17:48 +0530

    Added redirects

--- index.js
+++ index.js
@@ -11,9 +11,15 @@ app.get('/getApiRootUrl', (req, res) => {
   res.status(200).send(`${process.env.API_ROOT_URL}`);
 })
 
-app.get('/admin/*', (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, 'public', 'admin', 'index.html'));
-})
+app.use('/admin/*', function (req, res, next) {
+  res.sendFile(__dirname + '/public/admin/index.html');
+});
+app.use('/faculty/*', function (req, res, next) {
+  res.sendFile(__dirname + '/public/faculty/index.html');
+});
+app.use('/student/*', function (req, res, next) {
+  res.sendFile(__dirname + '/public/student/index.html');
+});
 
 app.listen(port, () => {
   console.log("Server is listening on port " + port);
