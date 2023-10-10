e6f168095c444885c621daaeeb116acbac786059
Akram <akramshafeek70@gmail.com>
2023-10-10 09:59:54 +0530

    Added continuous integration

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const path = require('path');
 const express = require('express');
 const app = express();
@@ -5,6 +6,10 @@ const app = express();
 const port = process.env.PORT || 4002;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/getApiRootUrl', (req, res) => {
+  res.status(200).send(`${process.env.API_ROOT_URL}`);
+})
 
 app.listen(port, () => {
   console.log("Server is listening on port " + port);
